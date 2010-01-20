aa93c254f3982535ccc40d79b4a0d5260ca2974b
Stephane Chomat <Stephane.chomat@imag.fr>
2010-01-20 16:09:56 +0100

    extension in progress

--- src/main/java/fr/imag/adele/fede/workspace/as/initmodel/InitModelLoadAndWrite.java
+++ src/main/java/fr/imag/adele/fede/workspace/as/initmodel/InitModelLoadAndWrite.java
@@ -5,6 +5,7 @@ import fr.imag.adele.cadse.core.var.ContextVariable;
 import fr.imag.adele.cadse.core.Item;
 import fr.imag.adele.cadse.core.ItemType;
 import fr.imag.adele.cadse.core.LogicalWorkspace;
+import fr.imag.adele.cadse.core.TypeDefinition;
 import fr.imag.adele.cadse.core.attribute.IAttributeType;
 import fr.imag.adele.fede.workspace.as.initmodel.jaxb.CValuesType;
 import fr.imag.adele.fede.workspace.as.initmodel.jaxb.ObjectFactory;
@@ -12,7 +13,7 @@ import fr.imag.adele.fede.workspace.as.initmodel.jaxb.ObjectFactory;
 public interface InitModelLoadAndWrite {
 
 	public abstract IAttributeType<?> loadAttributeDefinition(IInitModel initModel,
-			LogicalWorkspace theWorkspaceLogique, ItemType parent, CValuesType type, String cadseName)
+			LogicalWorkspace theWorkspaceLogique, TypeDefinition parent, CValuesType type, String cadseName)
 			throws CadseException;
 
 	public abstract void writeAttributeDefinition(ObjectFactory factory, ContextVariable cxt,
